3f103ed40cdfc99f39fc7d0e71fa9a61ef95bd94
Mathew Giljum <mathew.giljum@gmail.com>
2017-03-02 13:46:03 -0500

    Add activation script, calls and alt variant attempt

--- Win7ProActivator.bat
+++ Win7ProActivator.bat
@@ -3,31 +3,9 @@ REM  https://github.com/mgiljum/Windows-7-Pro-Activator
 :InstallOrNot
 cls
 @echo off
-setlocal ENABLEEXTENSIONS
 mode con: cols=100 lines=35
-echo   __        _____ _   _     _____     ____  ____   ___     
-echo " \ \      / /_ _| \ | |   |___  |   |  _ \|  _ \ / _ \    "
-echo "  \ \ /\ / / | ||  \| |      / /    | |_) | |_) | | | |   "
-echo "   \ V  V /  | || |\  |     / /     |  __/|  _ <| |_| |   "
-echo "    \_/\_/  |___|_| \_|    /_/      |_|   |_| \_\\___/    "
-echo       _    ____ _____ _____     ___  _____ ___  ____     ____   ____ ____  ___ ____ _____  
-echo "    / \  / ___|_   _|_ _\ \   / / \|_   _/ _ \|  _ \   / ___| / ___|  _ \|_ _|  _ \_   _| "
-echo "   / _ \| |     | |  | | \ \ / / _ \ | || | | | |_) |  \___ \| |   | |_) || || |_) || |   "
-echo "  / ___ \ |___  | |  | |  \ V / ___ \| || |_| |  _ <    ___) | |___|  _ < | ||  __/ | |   "
-echo " /_/   \_\____| |_| |___|  \_/_/   \_\_| \___/|_| \_\  |____/ \____|_| \_\___|_|    |_|   "
-echo.
-echo.
-echo   The use of this script assumes you have a device with valid 
-echo   installation or downgrade rights to Windows 7 Professional, 
-echo   or a valid Windows 7 product key. The use of this script  may be 
-echo   a violation of the Microsoft End User License Agreement or specific 
-echo   manufacturer agreements. The author assumes no responsibility for 
-echo   any misuse or any liability for any damages that may result from 
-echo   the use of this script.
-echo.
-echo.
-echo ///////////////////////////////////////////////////////////////////
-echo.
+type agreement.txt
+REM Detect if Windows 7?
 REM Detect current edition
 FOR /F "usebackq skip=2 tokens=1-5" %%A IN (`reg query "HKLM\Software\Microsoft\Windows NT\CurrentVersion" /v "ProductName" 2^>nul`) DO (
     set ValueName=%%A
@@ -38,7 +16,7 @@ echo.
 echo.
 REM Show current version and select license option
 :SELECTOPTION
-echo Current edition of Windows 7: %Edition%
+echo Installed edition of Windows 7: %EDITION%
 echo.
 echo.
 echo Select license type:
@@ -71,132 +49,12 @@ echo 10) Toshiba
 echo 11) Cancel
 echo.
 set /P MANUFACTURER="Enter selection: "
-if /i "%EDITION:~,1%" EQU "Professional" goto MANUPRO
-if /i "%EDITION:~,1%" EQU "Home Basic" goto MANUHOMEBASIC
-if /i "%EDITION:~,1%" EQU "Home Premium" goto MANUHOMEPREM
-if /i "%EDITION:~,1%" EQU "Ultimate" goto MANUULTIMATE
-:MANUPRO
-if /i "%MANUFACTURER:~,1%" EQU "1" goto Acer
-if /i "%MANUFACTURER:~,1%" EQU "2" goto Alienware
-if /i "%MANUFACTURER:~,1%" EQU "3" goto Asus
-if /i "%MANUFACTURER:~,1%" EQU "4" goto Dell
-if /i "%MANUFACTURER:~,1%" EQU "5" goto Fujitsu
-if /i "%MANUFACTURER:~,1%" EQU "6" goto HP
-if /i "%MANUFACTURER:~,1%" EQU "7" goto Lenovo
-if /i "%MANUFACTURER:~,1%" EQU "8" goto Samsung
-if /i "%MANUFACTURER:~,1%" EQU "9" goto Sony
-if /i "%MANUFACTURER:~,1%" EQU "10" goto Toshiba
-if /i "%MANUFACTURER:~,1%" EQU "11" goto InstallOrNot
-:MANUHOMEBASIC
-echo Support coming soon for Windows 7 %EDITION%
-pause
-exit
-:MANUHOMEPREM
-echo Support coming soon for Windows 7 %EDITION%
-pause
-exit
-:MANUULTIMATE
-echo Support coming soon for Windows 7 %EDITION%
-pause
-exit
-:Acer
-cscript //B "%windir%\system32\slmgr.vbs" -ilc C:\Certs\ACER-NEW.xrm-ms
-cscript //B "%windir%\system32\slmgr.vbs" -ipk YKHFT-KW986-GK4PY-FDWYH-7TP9F
-cscript //B "%windir%\system32\slmgr.vbs" -ato
-del C:\Certs /Q
-:Alienware
-cscript //B "%windir%\system32\slmgr.vbs" -ilc C:\Certs\ALIENWARE.xrm-ms
-cscript //B "%windir%\system32\slmgr.vbs" -ipk 4CFBX-7HQ6R-3JYWF-72GXP-4MV6W
-cscript //B "%windir%\system32\slmgr.vbs" -ato
-del C:\Certs /Q
-goto END
-:Asus
-cscript //B "%windir%\system32\slmgr.vbs" -ilc C:\Certs\ASUS-NEW.xrm-ms
-cscript //B "%windir%\system32\slmgr.vbs" -ipk 2WCJK-R8B4Y-CWRF2-TRJKB-PV9HW
-cscript //B "%windir%\system32\slmgr.vbs" -ato
-del C:\Certs /Q
-goto END
-:Dell
-cscript //B "%windir%\system32\slmgr.vbs" -ilc C:\Certs\DELL.xrm-ms
-cscript //B "%windir%\system32\slmgr.vbs" -ipk 32KD2-K9CTF-M3DJT-4J3WC-733WD
-cscript //B "%windir%\system32\slmgr.vbs" -ato
-del C:\Certs /Q
-goto END
-:Fujitsu
-cscript //B "%windir%\system32\slmgr.vbs" -ilc C:\Certs\FUJITSU.xrm-ms
-cscript //B "%windir%\system32\slmgr.vbs" -ipk PT9YK-BC2J9-WWYF9-R9DCR-QB9CK
-cscript //B "%windir%\system32\slmgr.vbs" -ato
-del C:\Certs /Q
-goto END
-:HP
-cscript //B "%windir%\system32\slmgr.vbs" -ilc C:\Certs\HP-COMPAQ.xrm-ms
-cscript //B "%windir%\system32\slmgr.vbs" -ipk 74T2M-DKDBC-788W3-H689G-6P6GT
-cscript //B "%windir%\system32\slmgr.vbs" -ato
-del C:\Certs /Q
-goto END
-:Lenovo
-cscript //B "%windir%\system32\slmgr.vbs" -ilc C:\Certs\IBM-LENOVO.xrm-ms
-cscript //B "%windir%\system32\slmgr.vbs" -ipk 237XB-GDJ7B-MV8MH-98QJM-24367
-cscript //B "%windir%\system32\slmgr.vbs" -ato
-del C:\Certs /Q
-goto END
-:Samsung
-cscript //B "%windir%\system32\slmgr.vbs" -ilc C:\Certs\SAMSUNG.xrm-ms
-cscript //B "%windir%\system32\slmgr.vbs" -ipk GMJQF-JC7VC-76HMH-M4RKY-V4HX6
-cscript //B "%windir%\system32\slmgr.vbs" -ato
-del C:\Certs /Q
-goto END
-:Sony
-cscript //B "%windir%\system32\slmgr.vbs" -ilc C:\Certs\SONY-NEW.xrm-ms
-cscript //B "%windir%\system32\slmgr.vbs" -ipk H9M26-6BXJP-XXFCY-7BR4V-24X8J
-cscript //B "%windir%\system32\slmgr.vbs" -ato
-del C:\Certs /Q
-goto END
-:Toshiba
-cscript //B "%windir%\system32\slmgr.vbs" -ilc C:\Certs\TOSHIBA.xrm-ms
-cscript //B "%windir%\system32\slmgr.vbs" -ipk 2V8P2-QKJWM-4THM3-74PDB-4P2KH
-cscript //B "%windir%\system32\slmgr.vbs" -ato
-del C:\Certs /Q
-goto END
+if /i "%MANUFACTURER:~,1%" EQU "11" goto Install
+cmd /c "installcert.bat"
 REM Install retail product key
 :INSTALLRETAIL
 cls
 set /P RETAILKEY="Enter retail/VLK or sticker key (WITH DASHES): "
 cscript //B "%windir%\system32\slmgr.vbs" -ipk %retailkey%
 cscript //B "%windir%\system32\slmgr.vbs" -ato
-goto END
-:END
-cls
-@echo off
-echo.
-echo.
-echo License installed, checking activation status...
-echo.
-@echo off
-for /f "tokens=3 delims=: " %%a in (
-    'cscript //nologo "%systemroot%\system32\slmgr.vbs" /dli ^| find "License Status:"' 
-) do set "licenseStatus=%%a"
-
-if /i "%licenseStatus%"=="Licensed" (
-  echo License activated successfully!
-  goto activationsuccess
-) else (
-  goto FAILEDRESTARTPROMPT )
-:FAILEDRESTARTPROMPT
-echo License activation failed. This may be because the certificate
-echo is not valid for your model or the wrong manufacturer was selected.
-echo.
-set /P restartprompt="Start over? (Y/N) "
-if /I "%restartprompt%" EQU "Y" goto RESTARTYES
-if /I "%restartprompt%" EQU "N" goto CANCEL
-:RESTARTYES
-GOTO :InstallOrNot
-pause
-:ACTIVATIONSUCCESS
-echo.
-echo.
-echo Press any key to exit...
-pause > nul
-cls
-:CANCEL
-exit /b
+cmd /c retailcheckstatus.bat
---
+++ checkstatus.bat
@@ -0,0 +1,70 @@
+:CHECKSTATUS
+cls
+@echo off
+echo.
+echo.
+echo License installed, checking activation status...
+echo.
+:RETAILCHECK
+@echo off
+for /f "tokens=3 delims=: " %%a in (
+    'cscript //nologo "%systemroot%\system32\slmgr.vbs" /dli ^| find "License Status:"' 
+) do set "licenseStatus=%%a"
+
+if /i "%licenseStatus%"=="Licensed" (
+  echo License activated successfully!
+  goto activationsuccess
+) else (
+  goto FAILEDRESTARTPROMPT )
+:FAILEDRESTARTPROMPT
+echo License activation failed. This may be due to a typo,
+echo or the product key you entered doesn't match the
+echo installed version.
+echo.
+set /P restartprompt="Start over? (Y/N) "
+if /I "%restartprompt%" EQU "Y" call Win7ProActivator.bat
+if /I "%restartprompt%" EQU "N" goto ACTIVATIONCOMPLETE
+:OEMCHECK
+@echo off
+for /f "tokens=3 delims=: " %%a in (
+    'cscript //nologo "%systemroot%\system32\slmgr.vbs" /dli ^| find "License Status:"' 
+) do set "licenseStatus=%%a"
+
+if /i "%licenseStatus%"=="Licensed" (
+  echo License activated successfully!
+  goto ACTIVATIONCOMPLETE
+) else (
+  goto TRYBVARIANT )
+:TRYBVARIANT
+if exist "C:\Certs\%MANU%\OEM\%EDITION% B\OEM\OEM.xrm-ms" (
+call "%EDITION%.bat" %InstallB%
+	) else (
+		goto UNSUPPORTED
+)
+:BVARIANTTEST
+@echo off
+for /f "tokens=3 delims=: " %%a in (
+    'cscript //nologo "%systemroot%\system32\slmgr.vbs" /dli ^| find "License Status:"' 
+) do set "licenseStatus=%%a"
+
+if /i "%licenseStatus%"=="Licensed" (
+  echo License activated successfully!
+  goto ACTIVATIONCOMPLETE 
+  ) else (
+	goto UNSUPPORTED
+)
+:UNSUPPORTED
+cls
+echo License activation failed. This may be due to an
+echo unspported image of Windows 7, or an unsupported make/model.
+set /P TRYAGAIN="Start over? (Y/N) "
+if /i TRYAGAIN=="y" call Win7ProActivator.bat
+if /i TRYAGAIN=="n" goto ACTIVATIONCOMPLETE
+:ACTIVATIONCOMPLETE
+echo.
+echo.
+echo Press any key to exit...
+pause > nul
+cls
+:CANCEL
+exit /b
