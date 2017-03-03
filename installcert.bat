1cbc189596ca7727fa39b8bc2e1cf394b77b0c58
Mathew Giljum <mathew.giljum@gmail.com>
2017-03-02 22:02:01 -0500

    Remove comments from testing

--- installcert.bat
+++ installcert.bat
@@ -16,24 +16,26 @@ if /i "%MANUFACTURER:~,1%" EQU "8" SET MANU=Samsung
 if /i "%MANUFACTURER:~,1%" EQU "9" SET MANU=Sony
 if /i "%MANUFACTURER:~,1%" EQU "10" SET MANU=Toshiba
 echo Manufacturer selected: %MANU% >> %~dp0log.txt
+
 :InstallA
 for /f %%a in (%~dp0Certs\%MANU%\%EDITION% A\OEM\slp.cmd) do (set keya=%%a)
-REM cscript //B "%windir%\system32\slmgr.vbs" -ilc "%~dp0Certs\%MANU%\%EDITION% A\OEM\OEM.xrm-ms"
+cscript //B "%windir%\system32\slmgr.vbs" -ilc "%~dp0Certs\%MANU%\%EDITION% A\OEM\OEM.xrm-ms"
 echo Certificate A installed.
-REM cscript //B "%windir%\system32\slmgr.vbs" -ipk %keya%
+cscript //B "%windir%\system32\slmgr.vbs" -ipk %keya%
 echo Product key A installed.
-REM cscript //B "%windir%\system32\slmgr.vbs" -ato
+cscript //B "%windir%\system32\slmgr.vbs" -ato
 echo Activation attempt A started.
 call "%~dp0checkstatus.bat" CHECKOEM
 exit /b
+
 :InstallB
 for /f %%a in (%~dp0Certs\%MANU%\%EDITION% B\OEM\slp.cmd) do (set keyb=%%a)
 echo License activation failed. Attempting alternative certificate and product key...
-REM cscript //B "%windir%\system32\slmgr.vbs" -ilc "%~dp0Certs\%MANU%\%EDITION% B\OEM\OEM.xrm-ms"
+cscript //B "%windir%\system32\slmgr.vbs" -ilc "%~dp0Certs\%MANU%\%EDITION% B\OEM\OEM.xrm-ms"
 echo Certificate B installed. >> %~dp0log.txt
-REM cscript //B "%windir%\system32\slmgr.vbs" -ipk %keyb%
+cscript //B "%windir%\system32\slmgr.vbs" -ipk %keyb%
 echo Product key B installed. >> %~dp0log.txt
-REM cscript //B "%windir%\system32\slmgr.vbs" -ato
+cscript //B "%windir%\system32\slmgr.vbs" -ato
 echo Activation attempt B started. >> %~dp0log.txt
 call "%~dp0checkstatus.bat" BVARIANTTEST
 exit /b
